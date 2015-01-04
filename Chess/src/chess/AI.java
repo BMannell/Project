64bc9a408ce0aaec684a5f1685ee25b9bc697416
d-johnston <djkj93@gmail.com>
2015-01-04 02:57:37 -0500

    worked on buidling game tree in AI class

--- Chess/src/chess/AI.java
+++ Chess/src/chess/AI.java
@@ -18,45 +18,46 @@ public class AI {
     
     public Move takeTurn(Piece[][] currentBoardState){
         Move myMove;
-        buildTree(currentBoardState);
+        constructTree(0, minimax, false);
         
         return null;
     }
     
     
-    // builds the minimax tree to be worked on
-    private void buildTree(Piece[][] board){
-        Node head = new Node(board, 0); // create the node that is the head of the tree
-        
-        
-        Node thisNode = minimax; // just a node pointer, start it at the head
-        ArrayList<int[]> pieces = findPieces(board); // fetch AI players piece locations
-        ArrayList<int[]> newPositions = null; // potential coordinates that a piece can move to
-        Piece[][] b; // going to be the board that results from a move being made
-        
-        // for each of AI's pieces
-        for(int i = 0; i < pieces.size(); i++){
-            newPositions = brain.getMoves(board, pieces.get(i)[0], pieces.get(i)[1]);
-            
-            // for each move possible
-            for(int j = 0; j < newPositions.size(); j++){
-                // get the board resulting from moving the pieve to that spot
-                b = brain.getBoardAfterMove(board, new Move(pieces.get(i)[0], pieces.get(i)[1], newPositions.get(j)[0], newPositions.get(j)[1]));
-                thisNode.children.add(new Node(b,fitnessEvaluation(b))); // get boardState after making the move
-            }
-        }
-    }
+    /*
+    recursive function to construct the tree
     
-    
-    
-    public void construct(int argPly, Node cur){
+    parameters:
+    - the number of plies moved down so far
+    - current node
+    - team making the moves
+    */
+    public void constructTree(int argPly, Node cur, boolean t){
         if(argPly == this.ply){
             return;
         }
+        ArrayList<int[]> pieces = brain.getPieces(cur.boardState, t);
+        ArrayList<int[]> newPlacements; // the list of potential places to move a piece 
+        Piece[][] boardFromMove = null; // board that results from executing a move
         
-       
+        for(int i = 0; i < pieces.size(); i++){ // for each piece owned on the board
+            newPlacements = brain.getMoves(cur.boardState, pieces.get(i)[0],pieces.get(i)[1]); // get moves for the current piece
+            
+            for(int j = 0; j < newPlacements.size(); j++){ // fir each of the new possible placements of this piece
+                // get the resulting board state
+                boardFromMove = brain.getBoardAfterMove(cur.boardState, new Move(pieces.get(i)[0], pieces.get(i)[1], newPlacements.get(j)[0], newPlacements.get(j)[1]));
+                
+                // and add it as a node to the current nodes list of children
+                cur.children.add(new Node(boardFromMove, fitnessEvaluation(boardFromMove))); // get boardState after making the move
+            }  
+        }
         
+        // once all children are in place, call this method on them for the opposite player and 1 ply deeper
+        for(int i = 0; i < cur.children.size(); i++){ // for each child node
+            constructTree(argPly+1,cur.children.get(i),!t);
+        }        
     }
+    
     
     // counts the pieces on the board that belong to the AI player
     private double fitnessEvaluation(Piece[][] b){
@@ -74,21 +75,7 @@ public class AI {
     }
     
     
-    // fetches a list of which pieces on the board belong to the player
-    // (used to know which pieces to make successors from)
-    private ArrayList<int[]> findPieces(Piece[][] board){
-        ArrayList<int[]> piecesList = new ArrayList();
-        
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                if(!board[i][j].team){ // belongs to the AI player
-                    piecesList.add(new int[]{j,i});
-                }
-            }
-        }
-        
-        return piecesList;
-    }
+    
     
     /*
     private int alphaBetaMax( int alpha, int beta, int depthleft ) {
@@ -116,6 +103,6 @@ public class AI {
         }
         return beta;
      }
-    
     */
+    
 }
