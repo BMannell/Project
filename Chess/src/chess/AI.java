b5011949e9e1c143ecdc0469708ae531260a30e8
d-johnston <djkj93@gmail.com>
2015-01-05 12:16:17 -0500

    - Fixed the build tree and node classes to old moves instead of board states in nodes. - Also the nodes still hold their evaluation suuuuu, meh

--- Chess/src/chess/AI.java
+++ Chess/src/chess/AI.java
@@ -16,7 +16,7 @@ public class AI {
     
     public Move takeTurn(Piece[][] currentBoardState){
         Move myMove;
-        constructTree(0, minimax, false);
+        constructTree(currentBoardState, 0, minimax, false);
         
         return null;
     }
@@ -31,36 +31,41 @@ public class AI {
     - current node
     - team making the moves
     */
-    public void constructTree(int argPly, Node cur, boolean t){
+    public void constructTree(Piece[][] board, int argPly, Node cur, boolean t){
         if(argPly == this.ply){
             return;
         }
-        ArrayList<int[]> pieces = Engine.getPieces(cur.boardState, t);
+        
+        ArrayList<int[]> pieces = Engine.getPieces(board, t);
         ArrayList<int[]> newPlacements; // the list of potential places to move a piece 
-        Piece[][] boardFromMove = null; // board that results from executing a move
+        Move moveFromPiece = null; // a move extracted from the piece
+        Piece[][] boardFromMoveFromPiece = null; // board that results from executing a move
         boolean checked = false;
         
         for(int i = 0; i < pieces.size(); i++){ // for each piece owned on the board
-            if(Engine.stateCheck(cur.boardState,t) != 0){
+            if(Engine.stateCheck(board,t) != 0){
                 checked = true;
             }
             else{
                 checked = false;
             }
-            newPlacements = Engine.getMoves(cur.boardState, pieces.get(i)[0],pieces.get(i)[1]); // get moves for the current piece
+            newPlacements = Engine.getMoves(board, pieces.get(i)[0],pieces.get(i)[1]); // get moves for the current piece
             
             for(int j = 0; j < newPlacements.size(); j++){ // fir each of the new possible placements of this piece
-                // get the resulting board state
-                boardFromMove = Engine.getBoardAfterMove(cur.boardState, new Move(pieces.get(i)[0], pieces.get(i)[1], newPlacements.get(j)[0], newPlacements.get(j)[1]));
+                // get a move that the piece can make
+                moveFromPiece = new Move(pieces.get(i)[0], pieces.get(i)[1], newPlacements.get(j)[0], newPlacements.get(j)[1]);
+                
+                // get the board that results from the move
+                boardFromMoveFromPiece = Engine.getBoardAfterMove(board, moveFromPiece);
                 
                 // and add it as a node to the current nodes list of children
-                cur.children.add(new Node(boardFromMove, fitnessEvaluation(boardFromMove))); // get boardState after making the move
-            }  
+                cur.children.add(new Node(moveFromPiece, fitnessEvaluation(boardFromMoveFromPiece))); // get boardState after making the move
+            }
         }
         
         // once all children are in place, call this method on them for the opposite player and 1 ply deeper
         for(int i = 0; i < cur.children.size(); i++){ // for each child node
-            constructTree(argPly+1,cur.children.get(i),!t);
+            constructTree(boardFromMoveFromPiece, argPly+1,cur.children.get(i),!t);
         }        
     }
     
