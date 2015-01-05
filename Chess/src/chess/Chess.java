dd758af0b9379bacdb4cb0d991188b9d1ad65967
BMannell <benjamin.mannell@gmail.com>
2015-01-05 18:57:01 -0500

    Game loop checked boolean

--- Chess/src/chess/Chess.java
+++ Chess/src/chess/Chess.java
@@ -7,7 +7,7 @@ public class Chess {
     public Piece[][] currentState = new Piece[8][8]; // the board
     AI ai;
     GUI gui;
-    
+    boolean playerChecked, aiChecked;
     public Chess(){
         gui = new GUI(this);
         //gui.displayMainMenu();
@@ -18,10 +18,11 @@ public class Chess {
         
         currentState = Engine.getBoardAfterMove(currentState, move);
         
-        boolean aiChecked = false;
+        aiChecked = false;
         switch(Engine.stateCheck(currentState, false)){
             case 0:
                 System.out.println("All-clear!");
+                aiChecked = false;
                 break;
             case 1:
                 System.out.println("Checked!");
@@ -38,7 +39,23 @@ public class Chess {
         //get ai turn
         Move aiMove = ai.takeTurn(currentState, aiChecked);
         currentState = Engine.getBoardAfterMove(currentState, aiMove);
-        //check gameover 
+        //check gameover
+        
+                switch(Engine.stateCheck(currentState, false)){
+            case 0:
+                System.out.println("All-clear!");
+                playerChecked = false;
+                break;
+            case 1:
+                System.out.println("Checked!");
+                playerChecked = true;
+                break;
+            case 2:
+                System.out.println("Checkmate!");
+                break;
+        }
+                
+        gui.underCheck = playerChecked;
         gui.drawBoard();
         
         gui.unlock();
